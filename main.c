391ee87c614bf505846c602eda23f03e5d23b058
lihf <lihf@genvict.com>
2019-02-17 10:09:53 +0800

    优化后最终的算法

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 struct record
 {
@@ -18,7 +19,7 @@ static int fine_min_num_and_count(char const *array, int len, struct record *res
         {
             min = array[i];
             
-            result->num = i;
+            result->num = i+1;
             result->count = min;
         }
     }
@@ -26,7 +27,7 @@ static int fine_min_num_and_count(char const *array, int len, struct record *res
 
 static int output_min_num(char const *src, int len)
 {
-    char signers[10] = {0};  //<lihf> 如果src记录的字符存在，数组signers[]则记录对应下标的数字出现的次数，0表示无
+    char signers[11] = {0};  //<lihf> 如果src记录的字符存在，数组signers[]则记录对应下标的数字出现的次数，0表示无
     char tmp;
     int ret_num = -1;
     
@@ -38,37 +39,33 @@ static int output_min_num(char const *src, int len)
     
     for(int i=0; i<len; i++)
     {
-        if( ('0' <= src[i]) && (src[i] <= '9') )
+        if( ('0' < src[i]) && (src[i] <= '9') )
         {
             tmp = src[i] - '0';
             signers[tmp]++;
         }
+        else if( '0' == src[i] )
+        {
+            signers[10]++;
+        }
     }
     
+    char *ptr = signers;
     struct record result;
-    fine_min_num_and_count(signers, 10, &result);
+    fine_min_num_and_count(ptr+1, 10, &result);
     
-    
-    if( (0 == result.num) &&  (0 == result.count) )
-    {//当0出现的个数为0的时候
-        struct record result_tmp;
-        char *ptr = signers;
-        
-        fine_min_num_and_count(ptr+1, 9, &result_tmp);
-        if(0 == result_tmp.count)
-        {//其他某个数字也出现0个
-            
-            ret_num = result_tmp.num+1;
-        }
-        else
-        {
-            ret_num = 1*10*(result.count+1);
-        }
+    if(10 == result.num)
+    {
+        ret_num = pow(10, result.count+1);
     }
     else
-    {//非零的值出现的个数为0
-        ret_num = result.num*(result.count+1);
-    }
+    {
+        ret_num = result.num;
+        for(int i=0; i<result.count; i++)
+        {
+            ret_num = ret_num*10 + result.num;
+        }
+    }    
     
     return ret_num;
 }
@@ -79,14 +76,22 @@ int main(void)
     char input[1024] = {0};
     int slen;
     
-    scanf("%s", input);
-    slen =  strlen(input);
-    
-    ret = output_min_num(input, slen);
-    if( ret > 0)
+    while(1)
     {
-        printf("%d\n", ret);
+        scanf("%s", input);
+        slen =  strlen(input);
+        
+        if( ('0' == input[0]) && (1 == slen) )
+        {
+            break;
+        }
+        
+        ret = output_min_num(input, slen);
+        if( ret > 0)
+        {
+            printf("%d\n", ret);
+        }
     }
-     
+    
     return 0;
 }
